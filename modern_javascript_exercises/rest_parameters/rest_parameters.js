0a2b2eaa10012f3a243bf89d5b588f88ec45840a
Jonald Damaso <damasojonald@gmail.com>
2023-01-31 10:48:03 +0000

    implemented rest_parameters

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,10 +1,11 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
-  // Add a loop here
-  return a + b;
+function add(...numbers) {
+  let sum = 0;
+  for (let n of numbers) sum += n;
+  return sum;
 }
 
-add(1, 2, 3, 4, 5);
+// add(1, 2, 3, 4, 5);
 
-module.exports = add;
+// module.exports = add;
