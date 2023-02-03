f55b5cc5a6152d8df0f44307d776633836ac07d7
Jonald Damaso <damasojonald@gmail.com>
2023-02-03 14:05:36 +0000

    done ternaries

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,15 +1,7 @@
 // Instructions can be found in ternaries.md
 
 export function sayHello(name) {
-  // if (name === undefined) {
-  //   return "Hello you!";
-  // } else return "Hello, " + name + "!";
   const result =  name  ? name : "you";
   console.log(result);
   return `Hello, ${result}!`;
 }
-
-// const greeting = (person) => {
-//   const name = person ? person.name : "stranger";
-//   return `Howdy, ${name}`;
-// }
